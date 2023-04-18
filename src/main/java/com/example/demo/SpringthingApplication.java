cf850781727b7c1e65624bce4caa3bd8d15c008b
SeanHarrisQA <sharris3@qa.com>
2023-04-18 16:01:40 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/SpringthingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Don't touch this class, need this annotation to run spring
+public class SpringthingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringthingApplication.class, args);
+	}
+
+}
