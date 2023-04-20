9f297628329cc97acb44c0ea28c8a5c2dcfd55bc
SeanHarrisQA <sharris3@qa.com>
2023-04-20 13:08:45 +0100

    All tests passing

--- src/test/java/com/example/demo/integration/CatIntegrationTest.java
+++ src/test/java/com/example/demo/integration/CatIntegrationTest.java
@@ -1,8 +1,14 @@
 package com.example.demo.integration;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,7 +53,60 @@ public class CatIntegrationTest {
 
 	@Test
 	void testGetAll() throws Exception {
-		Cat expectedCat = new Cat("Manny", true, true, 13);
+		List<Cat> xC = new ArrayList<>();
+		Cat expectedCat = new Cat(1L, "Manny", true, true, 13);
+		xC.add(expectedCat);
+		String xCAsJson = this.mapper.writeValueAsString(xC);
+
+		RequestBuilder req = get("/getAll");
+
+		ResultMatcher checkStatus = status().isOk();
+		ResultMatcher checkBody = content().json(xCAsJson);
+
+		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+	}
+
+	@Test
+	void testGetCat() throws Exception {
+		long id = 1L;
+		Cat xC = new Cat(id, "Manny", true, true, 13);
+		String xCAsJson = this.mapper.writeValueAsString(xC);
+
+		RequestBuilder req = get("/get/" + id);
+		ResultMatcher checkStatus = status().isOk();
+		ResultMatcher checkBody = content().json(xCAsJson);
+
+		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+	}
+
+	@Test
+	void testDeleteCat() throws Exception {
+		long id = 1L;
+		Cat xC = new Cat(id, "Manny", true, true, 13);
+		String xCAsJson = this.mapper.writeValueAsString(xC);
+
+		RequestBuilder req = delete("/remove/" + id);
+		ResultMatcher checkStatus = status().isOk();
+		ResultMatcher checkBody = content().json(xCAsJson);
+
+		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+	}
+
+	@Test
+	void testUpdateCat() throws Exception {
+		long id = 1L;
+		Cat xC = new Cat(id, "Moriarty", false, false, 10);
+		String xCAsJson = this.mapper.writeValueAsString(xC);
+		RequestBuilder req = patch("/update/" + id).queryParam("name", "Moriarty").queryParam("hasWhiskers", "false")
+				.queryParam("evil", "false").queryParam("length", "10");
+		// This is a scuffed way to do it, req is better than reqScuffed
+		RequestBuilder reqScuffed = patch("/update/" + id + "?name=Moriarty&evil=false&hasWhiskers=false&length=10");
+
+		ResultMatcher checkStatus = status().isOk();
+		ResultMatcher checkBody = content().json(xCAsJson);
+
+		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+
 	}
 
 }
